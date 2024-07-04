a239dfa359c9f715ee38664d07f458a78c87e04d
SAWASH <sawash3072@gmail.com>
2024-07-04 13:46:09 +0530

    Final

--- home.py
+++ home.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 pg = st.navigation([
     st.Page("page1.py", title="First page", icon="🔥"),
-    st.Page("page.py", title="Second page", icon=":material/favorite:"),
+    st.Page("page2.py", title="Second page", icon=":material/favorite:"),
 ])
 pg.run()
